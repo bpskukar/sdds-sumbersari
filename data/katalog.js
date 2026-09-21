c067cbddafa118aa133636d78fa71803c74abedc
mrafiraamadhan <mrafiraamadhan@gmail.com>
2026-09-21 18:36:11 +0800

    Update katalog.js

--- data/katalog.js
+++ data/katalog.js
@@ -85,7 +85,7 @@
        berjalan sebagai uji coba di browser masing-masing.
        --------------------------------------------------------------- */
     backend: {
-      url: "",
+            url: "https://script.google.com/macros/s/AKfycbyq-ZEcuI-JowbWZn3j8mUyoD5Lf7tbbmtRqsd3VqkcezaKjkkSvO3JEy3EOWmz3XLRlw/exec",
       /* Spreadsheet database (dibuka lewat tombol "Buka Sheet" di mode edit) */
       sheet: "https://docs.google.com/spreadsheets/d/1sAWFy-y3M0ea9Z5wEPF7fUetB7PqV8MpUAQ3qt24OUg/edit"
     },
